32673e284cfc2ea71362ec75ec29241b619d5bd9
Ben Richardson <benjiwildcat@gmail.com>
2023-05-01 23:51:34 -0500

    working on the index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let SVG = require('./lib/svg')
+const fs = require('fs')
+
+
+let example = new SVG()
+
+
+fs.writeFile('shape.svg', example.markup, function (err) {
+    if (err)
+        console.log(err)
+})
